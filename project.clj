9f28075d2a0885e5635ed661715c6f68e6070801
Darrell Hamilton <darrell.noice@gmail.com>
2015-01-19 23:21:40 -0700

    filler and version bump

--- project.clj
+++ project.clj
@@ -1,6 +1,6 @@
-(defproject darlyngames/ecs "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
+(defproject darlyngames/ecs "0.1.0"
+  :description "An Immutable Entity Component System"
+  :url "https://github.com/darlyngames/ecs"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.6.0"]])
